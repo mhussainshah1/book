53ae113553d795274076deaea76b5999e6796b4f
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-15 01:04:17 -0400

    Nav Bar

--- src/main/java/com/example/demo/Book.java
+++ src/main/java/com/example/demo/Book.java
@@ -9,25 +9,6 @@ import javax.persistence.Id;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
-/**
- * BookApplication 1
- * -----------------
- * Create a project in IntelliJ called BookApplication.
- *
- * Create a Book class that allows for Book title, author, description, price and isInStock variables
- *
- * The Book class shall contain private member variables, a default constructor, an overloaded constructor and a method
- * called getDisplayText() which prints the author, title and description.
- *
- *
- * The application shall also contain getters and setters for each private member variable.
- *
- * Create another class called BookApp which contains the main method. In the main method you shall create an instance
- * of a Book and print the Author, Title and Description.
- *
- * Publish your solution to GitHub and submit your GitHub url as the assignment.
- *
- */
 @Entity
 public class Book {
     @Id
