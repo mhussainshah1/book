df023615e9fb6796b6a47036b1c33477bc692c7c
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-15 00:49:42 -0400

    delete button

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Iterator;
@@ -57,9 +54,11 @@ public class HomeController {
         return "redirect:/";
     }
 
-    @RequestMapping("/deleteEntities/{entities}")
-    public String delBooks(@PathVariable("entities") Iterable<Book> entities){
-        bookRepository.deleteAll(entities);
+    @RequestMapping("/delete")
+    public String delBooks(@RequestParam("check") long[] ids){
+        for(long id : ids){
+            bookRepository.deleteById(id);
+        }
         return "redirect:/";
     }
 
